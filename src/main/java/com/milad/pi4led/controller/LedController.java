d389c1b40c4d737a60a96a2a9e034131908836e5
Milad ARDEHALI <milad.ardehali@gmail.com>
2018-02-11 17:17:22 +0100

    changement etat pinstate gpio 25

--- src/main/java/com/milad/pi4led/controller/LedController.java
+++ src/main/java/com/milad/pi4led/controller/LedController.java
@@ -18,7 +18,7 @@ public class LedController {
     public String light() {
         if (pin == null) {
             GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_19,"MyLED", PinState.LOW);
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25,"MyLED", PinState.LOW);
         }
 
         pin.toggle();
