ab17965a88b1f38bdd804c9e10e9fd3476223a95
Milad ARDEHALI <milad.ardehali@gmail.com>
2018-02-11 18:02:49 +0100

    ajout de fonctionnalite GPIO

--- src/main/java/com/milad/pi4led/controller/LedController.java
+++ src/main/java/com/milad/pi4led/controller/LedController.java
@@ -14,16 +14,53 @@ public class LedController {
         return "Hello world!";
     }
 
-    @RequestMapping("/light")
-    public String light() {
-        if (pin == null) {
-            GpioController gpio = GpioFactory.getInstance();
-            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25,"MyLED", PinState.LOW);
-        }
+    @RequestMapping("/toggle")
+    public String toggle() {
 
-        pin.toggle();
+        getPin().toggle();
 
         return "OK";
+
+    }
+
+    private String checkState() {
+        return (getPin().isHigh() ? "Light is on" : "Light is off");
+    }
+
+    @RequestMapping("/on")
+    public String on() {
+        getPin().high();
+
+        return checkState();
+    }
+
+    @RequestMapping("/off")
+    public String off() {
+        getPin().low();
+
+        return checkState();
+    }
+
+    @RequestMapping("/blink")
+    public String blink() {
+        getPin().blink(200L, 2000L);
+        return "Light is blinking";
+    }
+
+    @RequestMapping("/pulse")
+    public String pulse() {
+        getPin().pulse(5000L);
+        return "Light is pulsing";
+    }
+
+    public GpioPinDigitalOutput getPin() {
+
+        if (pin == null) {
+            GpioController gpio = GpioFactory.getInstance();
+            pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "MyLED", PinState.LOW);
+        }
+
+        return pin;
     }
 
 }
